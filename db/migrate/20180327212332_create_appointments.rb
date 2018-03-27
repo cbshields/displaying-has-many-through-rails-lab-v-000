7b671225e82b29997a49f17aa1fadb05431d3078
Caroline Shields <cbshields@outlook.com>
2018-03-27 21:24:18 +0000

    automatically backed up by learn

--- db/migrate/20180327212332_create_appointments.rb
+++ db/migrate/20180327212332_create_appointments.rb
@@ -2,8 +2,8 @@ class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.patient :
-      t.doctor :
+      t.belongs_to :patient
+      t.belongs_to :doctor
     end
   end
 end
